250864fad78065c2d4d3a6000e48244b947b98fd
TanyEm <tanyem@protonmail.com>
2021-02-18 22:14:32 +0200

    Get cells without img

---
+++ Flicker App/Views/SearchBarView.swift
@@ -0,0 +1,13 @@
+//
+//  SearchBarView.swift
+//  Flicker App
+//
+//  Created by Tatiana Podlesnykh on 19.2.2021.
+//
+
+import UIKit
+
+class SearchBarView: UICollectionReusableView {
+
+
+}
--- Flicker App/Views/SearchViewController.swift
+++ Flicker App/Views/SearchViewController.swift
@@ -7,8 +7,9 @@
 
 import UIKit
 
-
 class SearchViewController: UIViewController {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
     
     private let viewModel = PhotosViewModel()
     private var photosList = [PhotoData]()
@@ -33,15 +34,57 @@ class SearchViewController: UIViewController {
             DispatchQueue.main.async {
                 if list.photo.count != 0 {
                     for photo in list.photo {
-                        print(photo.id)
-                        self.testArray[photo.id!] = "pencil"
+                        print(photo.title)
+                        self.photosList.append(PhotoData(id: photo.id, secret: photo.secret, server: photo.server, title: photo.title))
                     }
+                    self.collectionView.reloadData()
                 } else {
                     print("Error: failed to get restaurant list")
                 }
             }
         }
         
+    }
+    
+   
+
+}
+
+extension SearchViewController: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return photosList.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell: PhotoCellView = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! PhotoCellView
+        
+        if let unwrappedCost = photosList[indexPath.item].title{
+            cell.title.text = String(describing: unwrappedCost)
+        }else{
+        cell.title.text = "0" //Your Value If It can't Find Cost in Class
+        }
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let yourWidth = collectionView.bounds.width/4.0
+        let yourHeight = yourWidth
+
+        return CGSize(width: yourWidth, height: yourHeight)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets.zero
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
     }
 }
 
