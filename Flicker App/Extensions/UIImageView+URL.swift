b26709c7dda5c508d13bdb621e1eee8bbe711ce1
TanyEm <tanyem@protonmail.com>
2021-02-19 10:58:47 +0200

    Add activity indicator

--- Flicker App/Extensions/UIImageView+URL.swift
+++ Flicker App/Extensions/UIImageView+URL.swift
@@ -8,8 +8,37 @@
 import UIKit
 
 extension UIImageView {
+    
+    private var activityIndicator: UIActivityIndicatorView {
+            let activityIndicator = UIActivityIndicatorView()
+            activityIndicator.hidesWhenStopped = true
+            activityIndicator.color = UIColor.black
+            self.addSubview(activityIndicator)
+
+            activityIndicator.translatesAutoresizingMaskIntoConstraints = false
+
+            let centerX = NSLayoutConstraint(item: self,
+                                             attribute: .centerX,
+                                             relatedBy: .equal,
+                                             toItem: activityIndicator,
+                                             attribute: .centerX,
+                                             multiplier: 1,
+                                             constant: 0)
+            let centerY = NSLayoutConstraint(item: self,
+                                             attribute: .centerY,
+                                             relatedBy: .equal,
+                                             toItem: activityIndicator,
+                                             attribute: .centerY,
+                                             multiplier: 1,
+                                             constant: 0)
+            self.addConstraints([centerX, centerY])
+            return activityIndicator
+        }
+    
     func downloaded(from url: URL, contentMode mode: UIView.ContentMode = .scaleAspectFill) {
         contentMode = mode
+        let activityIndicator = self.activityIndicator
+        activityIndicator.startAnimating()
         URLSession.shared.dataTask(with: url) { data, response, error in
             guard
                 let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200,
@@ -19,7 +48,10 @@ extension UIImageView {
                 else { return }
             DispatchQueue.main.async() { [weak self] in
                 self?.image = image
+                activityIndicator.stopAnimating()
+                activityIndicator.removeFromSuperview()
             }
+            
         }.resume()
     }
     func downloaded(from link: String, contentMode mode: UIView.ContentMode = .scaleAspectFill) {
